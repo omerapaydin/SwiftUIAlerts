2dd6bf64d22fcae6cc8f2644a4d1c96369125531
Ömer Apaydın <omerapaydin90@gmail.com>
2026-07-12 18:22:43 +0300

    ActionSheet

--- SwiftUIAlerts/ContentView.swift
+++ SwiftUIAlerts/ContentView.swift
@@ -8,12 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var alert = false
+    @State private var actionSheet = false
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button("Alert"){
+                alert = true
+            }.alert("Başlık",isPresented: $alert,actions: {
+                Button("İptal",role: .cancel,action: {
+                    print("İptal Seçildi")
+                })
+                Button("Tamam",role: .destructive,action: {
+                    print("Tamam Seçildi")
+                })
+            },message: {
+                Text("İçerik")
+            })
+            .padding()
+            
+            
+            Button("Action Sheet"){
+                actionSheet = true
+            }.actionSheet(isPresented: $actionSheet){
+                ActionSheet(title: Text("Başlık"),message: Text("İçerik"),
+                     buttons: [
+                        .default(Text("İptal"),action: {
+                            print("iptaş")
+                        }),
+                        .destructive(Text("Tamam"),action: {
+                            print("tamam seçildi")
+                        })
+                        
+                     ]
+                )
+            }.padding()
+            
+            Button("Context Menu"){
+                
+            }.padding()
+            
+            
+            
+            
         }
         .padding()
     }
