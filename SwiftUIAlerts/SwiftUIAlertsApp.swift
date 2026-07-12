7a248b8b49b21730c7d42b036ea86e6da715bd95
Ömer Apaydın <omerapaydin90@gmail.com>
2026-07-12 17:50:41 +0300

    Initial Commit

---
+++ SwiftUIAlerts/SwiftUIAlertsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAlertsApp.swift
+//  SwiftUIAlerts
+//
+//  Created by Ömer Apaydın on 12.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAlertsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
